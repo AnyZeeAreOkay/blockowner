afc7ab8914f27c7bf7e4e1ade63ee2184c373192
AnyZeeAreOkay <48224118+AnyZeeAreOkay@users.noreply.github.com>
2020-04-29 16:56:54 -0400

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,11 @@
+minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
+        minetest.get_meta(pos):set_string("oldnode", oldnode.name)
+        minetest.get_meta(pos):set_string("owner", placer:get_player_name())
+
+end)
+
+minetest.register_on_dignode(function(pos, oldnode, digger)
+    minetest.get_meta(pos):set_string("oldnode", oldnode.name)
+    minetest.get_meta(pos):set_string("digger", digger:get_player_name())
+
+end)
