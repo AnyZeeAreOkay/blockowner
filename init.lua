6b59e34322a33a26c7de17169b08a401f2835bc4
AnyZeeAreOkay <48224118+AnyZeeAreOkay@users.noreply.github.com>
2020-05-01 00:51:57 -0400

    Update init.lua

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack
 end)
 
 minetest.register_on_dignode(function(pos, oldnode, digger)
+    if digger == nil then return end
     minetest.get_meta(pos):set_string("oldnode", oldnode.name)
     minetest.get_meta(pos):set_string("digger", digger:get_player_name())
 
